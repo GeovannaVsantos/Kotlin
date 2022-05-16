ffcaa2ef264837cb54012c9591c22a59e7043b84
Geovanna Vitoria S. dos Santos <geovannavsantos2018@gmail.com>
2022-05-16 11:18:02 -0300

    First Version

---
+++ src/main/kotlin/IMC.kt
@@ -0,0 +1,25 @@
+fun main () {
+
+    //Definir a área do diametro
+
+    //Raio metade do diametro
+
+    println("Digite o diametro")
+    var diametro = readln().toDouble()
+
+    var pi = 3.14
+    var d2 = diametro * diametro
+
+    println("A area do Diametro é: ${pi * (d2/4)}")
+
+    //Definir o perimetro do diametro
+
+    println("Agora vamos definir o perimetro")
+    print("O perimetro do diametro é ${diametro * pi}")
+
+
+
+
+
+
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import java.util.*
+fun main(){
+
+    val reader = Scanner(System.`in`)
+
+    println("Police Departament. \nWhats your emergency")
+
+    val bang = reader.nextLine()
+
+    println("Ok, me fale seu número")
+
+    val chan = reader.nextDouble()
+
+    println("Me diga seu endereço")
+
+    var mark = readLine()
+
+    println("Respire e mantenha a calma")
+
+    var nct = readLine()
+
+
+    println("The police is coming")
+
+}
+
