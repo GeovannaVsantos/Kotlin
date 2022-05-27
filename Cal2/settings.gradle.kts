2b4fc7a98f277ef2e2a5b85e1fd2d4704cbcea89
Geovanna Vitoria S. dos Santos <geovannavsantos2018@gmail.com>
2022-05-26 23:00:24 -0300

    Unit Calculadora

---
+++ Cal2/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Cal2"
+
---
+++ Cal2/src/test/kotlin/CalculadoraTest.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CalculadoraTest {
+    @Test
+    fun soma() {
+        assertEquals(3)
+    }
+
+
+}
+}
